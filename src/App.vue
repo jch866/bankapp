bf559d02a0021b3ed023a3bcce6dd1f4d0dd4349
jch866 <jch866@126.com>
2023-07-29 14:54:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="box">1231
+    <h1>123</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 100px;
+  height: 100px;
+  background: gray;
+  h1 {
+    color: red;
+  }
+}
+</style>
