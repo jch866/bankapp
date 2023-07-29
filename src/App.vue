7617f85277869a8ac3bd258cb9960e1d76c82788
jch866 <jch866@126.com>
2023-07-29 16:49:28 +0800

    feat: 项目全局集成element plus ; 添加组件中文支持

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <el-button type="danger" :icon="Plus">123</el-button>
+  <el-pagination
+    :page-size="20"
+    :pager-count="11"
+    layout="prev, pager, next"
+    :total="1000"
+  ></el-pagination>
 </template>
-
+<script setup lang="ts">
+import { Plus } from "@element-plus/icons-vue";
+</script>
 <style scoped lang="scss"></style>
