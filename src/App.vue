a233a1fc72f875cd6479ef74c92fff609d44bba5
jch866 <jch866@126.com>
2023-07-29 16:00:16 +0800

    feat: 安装husky  commit lint

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div class="box">1231
-    <h1>123</h1>
-  </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped lang="scss">
-.box {
-  width: 100px;
-  height: 100px;
-  background: gray;
-  h1 {
-    color: red;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
