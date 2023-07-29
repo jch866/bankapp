1924f58547a8376401f33e083b22513ba8a04afe
jch866 <jch866@126.com>
2023-07-29 22:14:08 +0800

    feat: 环境变量配置

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,6 @@
 </template>
 <script setup lang="ts">
 import { Plus } from "@element-plus/icons-vue";
-import Test from '@/components/test.vue'
+import Test from "@/components/test.vue";
 </script>
 <style scoped lang="scss"></style>
